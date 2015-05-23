fb5b38ce076a3ba4826ad3f7515a5997e8ded354
Peter Henry <phenry@caltech.edu>
2015-05-22 21:49:27 -0500

    All indentation converted to 4 spaces

--- js/directives/programListing.js
+++ js/directives/programListing.js
@@ -1,9 +1,9 @@
 app.directive('programListing', function() {
-	return {
-		restrict: 'E',
-		scope: {
-			listing: '=',
-		},
-		templateUrl: 'js/directives/programListing.html'
-	};
+    return {
+        restrict: 'E',
+        scope: {
+            listing: '=',
+        },
+        templateUrl: 'js/directives/programListing.html'
+    };
 });
